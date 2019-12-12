c92c7570ba537d0429d04bc82eb73a9535af06d8
Nicholas Reith <nreith@users.noreply.github.com>
2019-12-11 20:57:51 -0600

    Create elementary_os.sh

---
+++ elementary_os.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Elementary OS Post-Installation Script
+
+#1. OS tweaks
+
+ # - Turn off trackpad naturall scrolling
+ # - 
+ # - 
+
+2. # icons
+    sudo add-apt-repository ppa:papirus/papirus
+    sudo apt-get update
+    sudo apt-get install papirus-icon-theme
