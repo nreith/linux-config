09b6b0763133ad0c986e759910f93693c13ece79
Nicholas Reith <nreith@users.noreply.github.com>
2019-12-19 12:00:13 -0600

    Update xubuntu.sh

--- xubuntu.sh
+++ xubuntu.sh
@@ -3,18 +3,18 @@
 # Xubuntu config and installs
 
 # install fonts
-    sudo apt-get install -y ttf-mscorefonts-installer --quiet
-    echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections
-    # cp -r ~/dotfiles/.fonts/  ~/.fonts/
+sudo apt-get install -y ttf-mscorefonts-installer --quiet
+echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections
+# cp -r ~/dotfiles/.fonts/  ~/.fonts/
 
 # install icons
 
 sudo add-apt-repository ppa:papirus/papirus
 sudo apt-get update
-sudo apt-get install papirus-icon-theme libreoffice-style-papirus
+sudo apt-get install -y papirus-icon-theme libreoffice-style-papirus filezilla-theme-papirus
 
 # stilo themes
-sudo apt install -y git
+sudo apt install -y gtk2-engines-murrine gtk2-engines-pixbuf fonts-roboto ninja-build git meson sassc
 git clone https://github.com/lassekongo83/stilo-themes.git
 cd stilo-themes
 meson build
