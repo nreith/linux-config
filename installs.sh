20eb610ec7aab1a6d8f82a5dec2dec0102824b57
Nicholas Reith <nreith@users.noreply.github.com>
2019-12-19 15:02:11 -0600

    Update installs.sh

--- installs.sh
+++ installs.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-function up() {}
-    sudo apt-get update
-    sudo apt-get upgrade -y
-    sudo apt-get autoremove -y
+function up() {
+    sudo apt update
+    sudo apt upgrade -y
+    sudo apt autoremove -y
     }
 
 # common
 
 function install_common() {
-    sudo apt-get install \
+    sudo apt install -y \
         build-essential \
+	devhelp zeal \
+	git \
         guake \
+	firefox \
+	chromium-browser \
+	pass \
         python-pip \
         screenfetch \
+	slack \
         ssh openssh-server \
         ubuntu-restricted-extras \
         transmission \
@@ -25,16 +31,16 @@ function install_common() {
 function install_anaconda3() {
     cd /tmp && wget https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh -O Anaconda3-2019.10-Linux-x86_64.sh
     echo "46d762284d252e51cd58a8ca6c8adc9da2eadc82c342927b2f66ed011d1d8b53" Anaconda3-2019.10-Linux-x86_64.sh | sha256sum -c -
-    bash Anaconda3-2019.10-Linux-x86_64.sh -b -p /opt/
+    sudo mkdir /opt/anaconda3
+    bash Anaconda3-2019.10-Linux-x86_64.sh -b /opt/anaconda3
+    sudo chown -R $USER:$USER /opt/anaconda3
     echo 'export PATH=$PATH:/opt/anaconda3/bin' >> ~/.bashrc
     source ~/.bashrc
     }
 
 # Azure Data Studio
 function install_azdatastudio() {
-    sudo apt-get install libxss1
-    sudo apt-get install libgconf-2-4
-    sudo apt-get install libunwind8 
+    sudo apt -y install libxss1 libgconf-2-4 libunwind8 
     cd /tmp && wget https://azuredatastudiobuilds.blob.core.windows.net/releases/1.13.1/azuredatastudio-linux-1.13.1.deb
     sudo dpkg -i azuredatastudio-linux-1.13.1.deb
     }
@@ -43,25 +49,24 @@ function install_azdatastudio() {
 function install_chrome() {
     wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
     sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-    sudo apt-get update 
-    sudo apt-get install -y google-chrome-stable
+    sudo apt update 
+    sudo apt install -y google-chrome-stable
     }
 
 # dbeaver
 function install_dbeaver() {
     wget -O - https://dbeaver.io/debs/dbeaver.gpg.key | sudo apt-key add -
     echo "deb https://dbeaver.io/debs/dbeaver-ce /" | sudo tee /etc/apt/sources.list.d/dbeaver.list
-    sudo apt-get update && sudo apt-get install dbeaver-ce
     sudo add-apt-repository ppa:serge-rider/dbeaver-ce
-    sudo apt-get update
-    sudo apt-get install -y dbeaver-ce
+    sudo apt update
+    sudo apt install -y dbeaver-ce
     }
 
 # docker
 function install_docker() {
-    sudo apt-get update
-    sudo apt-get remove docker docker-engine docker.io
-    sudo apt-get install \
+    sudo apt update
+    sudo apt remove docker docker-engine docker.io
+    sudo apt install -y \
         apt-transport-https \
         ca-certificates \
         curl \
@@ -69,33 +74,25 @@ function install_docker() {
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
     sudo apt-key fingerprint 0EBFCD88
     sudo add-apt-repository \
-        "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-    sudo apt install docker.io
-    sudo apt-get update
-    sudo apt-get install docker-ce
-    sudo usermod -aG docker $MYUSER
+        "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+    sudo apt install -y docker.io
+    sudo apt update
+    sudo apt install -y docker-ce
+    sudo usermod -aG docker $USER
     }
 
 # free office
-function install_Freeoffice() {
-    sudo apt-get remove libre-office # if installed
+function install_freeoffice() {
+    sudo apt remove libre-office # if installed
     cd /tmp && wget https://www.softmaker.net/down/softmaker-freeoffice-2018_973-01_amd64.deb
     sudo dpkg -i softmaker-freeoffice*.deb
     sudo /usr/share/freeoffice2018/add_apt_repo.sh
     }
 
-# git
-function install_git() {
-    sudo apt-get install git \
-        gitk \ # Graphical history tool
-        git-gui # Graphical commit tool
-    }
-
 # java 8
 function install_java8() {
-    sudo add-apt-repository ppa:webupd8team/java
-    sudo apt-get update
-    sudo apt-get install oracle-java8-installer
+    sudo apt update
+    sudo apt install -y openjdk-8-jdk
     }
 
 # pycharm
@@ -109,15 +106,15 @@ function install_pycharm() {
 function install_spotify() {
     curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
     echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-    sudo apt-get update && sudo apt-get install -y spotify-client
+    sudo apt update && sudo apt install -y spotify-client
     }
 
 # Sublime text
 function install_sublimetext() {
     wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
     echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-    sudo apt-get update
-    sudo apt-get install -y sublime-text
+    sudo apt update
+    sudo apt install -y sublime-text
     }
 
 function install_vbox_vagrant() {
@@ -139,4 +136,5 @@ function cleanup() {
     sudo apt -y autoclean
     sudo apt -y autoremove
     sudo apt -y autoclean
+    sudo rm -rf /tmp/*
     }
