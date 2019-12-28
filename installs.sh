acf70a0637f2fc18ccd0fdbbb28e7f495ade3d99
Nicholas Reith <nreith@users.noreply.github.com>
2019-12-27 22:45:23 -0600

    Update installs.sh

--- installs.sh
+++ installs.sh
@@ -101,8 +101,7 @@ function install_postman() {
 	cd /tmp && wget https://dl.pstmn.io/download/latest/linux64 -O postman-linux-x64.tar.gz
 	sudo tar -xvzf postman-linux-x64.tar.gz -C /opt
 	mkdir ~/.local/share/applications && touch ~/.local/share/applications/postman2.desktop
-	cat << EOF > ~/.local/share/applications/postman2.desktop
-	[Desktop Entry]
+	printf '[Desktop Entry]
 	Name=Postman
 	GenericName=API Client
 	X-GNOME-FullName=Postman API Client
@@ -112,8 +111,7 @@ function install_postman() {
 	Terminal=false
 	Type=Application
 	Icon=/opt/Postman/app/resources/app/assets/icon.png
-	Categories=Development;Utilities;
-	EOF
+	Categories=Development;Utilities;' > ~/.local/share/applications/postman2.desktop
 	sudo ln -s /opt/Postman/Postman /usr/bin/postman
 }
 
