15d13012ab4c205f84bfde75d16241bbf50ee510
Nicholas Reith <nreith@users.noreply.github.com>
2019-12-31 12:24:52 -0600

    Update quicktile_elementary_os.sh

--- scripts/quicktile_elementary_os.sh
+++ scripts/quicktile_elementary_os.sh
@@ -2,6 +2,11 @@
 
 # Setup keyboard window tiling for Elementary OS
 
+# Instructions:
+# 1. wget or otherwise download this shell script
+# 2. Run script as sudo: sudo bash quicktile_elementary_os.sh
+# 3. Log out and log back in.
+
 # Installs quicktile (https://github.com/ssokolow/quicktile) 
 # for Elementary OS 5.1 Hera (https://elementary.io/)
 # based on Ubuntu 18.04 LTS (http://releases.ubuntu.com/18.04/)
@@ -48,39 +53,61 @@ sudo apt-get install gnome-themes-standard
 # As a side-benefit, this also fixes the same issue if you want to use i3 (for example, regolith-desktop) as a window
 # manager on top of Elementary OS. I personally switch back and forth between i3 and Pantheon with Elementary as the base.
 
+# Backup config file
 cd /usr/share/themes/elementary/gtk-3.0/
 sudo cp gtk-widgets.css gtk-widgets.css.bak
-sudo sed -i 's/^decoration {\n.*}
 
-
-decoration {
+# Replace old section with new section
+printf 'decoration {
     border-radius: 4px 4px 0 0;
     box-shadow:
         0 0 0 1px @decoration_border_color,
         0 14px 28px rgba(0, 0, 0, 0.35),
         0 10px 10px rgba(0, 0, 0, 0.22);
     margin: 12px;
-}
+}' > /tmp/old.txt
 
-/*decoration {
-    border-radius: 4px 4px 0 0;
-    box-shadow:
-        0 0 0 1px @decoration_border_color,
-        0 14px 28px rgba(0, 0, 0, 0.35),
-        0 10px 10px rgba(0, 0, 0, 0.22);
-    margin: 12px;
-}*/
-
-decoration {
+printf 'decoration {
 	box-shadow: none;
 	border: none;
 	padding: 0;
 	margin: 0;
-}
+}' > /tmp/new.txt
 
+contents=$(< gtk-widgets.css)
+old=$(< /tmp/old.txt)
+new=$(< /tmp/new.txt)
 
+# Replace conf file with new text instead of old
+echo "${contents/$old/$new}" > gtk-widgets.css
+
+# Clean up tmp files
+rm /tmp/old.txt /tmp/new.txt
 
 # Run quicktile once to generate config
 quicktile
+
+# Remove "KP_" from keyboard shortcuts in ~/.config/quicktile.cfg
+# Because I don't have a numeric keypad
+# I'll leave all other shortcuts as defaults, just using top number keys instead
+sed -i 's/^KP\_//g' ~/.config/quicktile.cfg
+
+# Oh, and a quick fix because it doesn't like the "Enter" shortcut for monitor-switch
+# now that it's not the keypad enter button
+# Now ctrl+alt+space switches the window to another monitor
+sed -i 's/^Enter /Space /g' ~/.config/quicktile.cfg
+
+# Phew. Almost done!
+# Just need to make quicktile startup automatically when you log in
+printf '
+[Desktop Entry]
+Name[en_US]=quicktile
+Comment[en_US]=quicktile --daemonize
+Exec=quicktile --daemonize
+Icon=application-default-icon
+X-GNOME-Autostart-enabled=true
+Type=Application' > ~/.config/autostart/quicktile.desktop
+
+# Now log out and log back in and tiling should be working
 
 
