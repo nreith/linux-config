277aebde8a3602dc594d6f5f5cbb76a2b2c3f7bf
Nicholas Reith <nreith@users.noreply.github.com>
2020-01-01 14:50:57 -0600

    Update nick_installs.sh

--- scripts/nick_installs.sh
+++ scripts/nick_installs.sh
@@ -12,11 +12,12 @@ wget https://raw.githubusercontent.com/nreith/linux_config/master/scripts/instal
 source install_functions.sh
 
 # installs
-updates
-install_common
+run_updates
+install_common_cli
+install_common_gui
+config_locale_tz
 install_anaconda3
 install_azdatastudio
-install_chrome
 install_dbeaver
 install_docker
 install_java8
@@ -25,4 +26,4 @@ install_pycharm
 install_spotify
 install_sublimetext
 install_vscode
-cleanup
+run_cleanup
